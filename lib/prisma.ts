21276550e8c8b00a28cf1aaf25b2e3c2545c4d30
Ícaro Teodoro <icaro.teodoro84@gmail.com>
2026-03-01 21:42:08 -0300

    refactor: Update Prisma to use `@prisma/adapter-libsql` directly and adjust Next.js development type reference path.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,9 @@
-import { PrismaClient } from './generated/prisma/client';
-import { createClient } from '@libsql/client'
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 
-const libsql = createClient({
+const adapter = new PrismaLibSql({
     url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
 })
 
-const adapter = new PrismaBetterSqlite3(libsql)
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({ adapter })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = new PrismaClient({ adapter })
